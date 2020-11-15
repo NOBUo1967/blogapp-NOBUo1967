895be75bc3f58f3530948e1b4cca07b89b0f008c
Shin H <nobunegi13@gmail.com>
2020-11-16 06:52:35 +0900

    followerの記事がtimelineｎ表示されるようにした。

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,4 +1,12 @@
 class TimelinesController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    user_ids = current_user.followings.pluck(:id)
+    # pluck = followingsの取得できたrecodeのidをとれる
+    # ex) User1 のfollowingがuser2,user3,user4の場合 => [2,3,4]
+    @articles = Article.where(user_id: user_ids)
+    #user_idがuser_idsに含まれているやつを探してくればいい。
+    #puclkでArrayをわたすと、or検索で値を渡してくれる。
   end
 end
