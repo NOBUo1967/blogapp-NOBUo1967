ab009b13a70e1f46036615f44655da033b3f4dee
Shin H <nobunegi13@gmail.com>
2020-12-21 12:47:07 +0900

    ダミー記事のcontentが空のため作成し直した。

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-john = User.create!(email: 'john@example.com', password: 'password')
-emily = User.create!(email: 'emily@wxample.com', password: 'password')
+ada = User.create!(email: 'ada@example.com', password: 'password')
+carlos = User.create!(email: 'carlos@wxample.com', password: 'password')
 
 5.times do
-    john.articles.create(
-        title: Faker::Lorem.sentence(word_count: 5),
-        content: Faker::Lorem.sentence(word_count: 100)
+    ada.articles.create(
+      title: Faker::Lorem.sentence(word_count: 5),
+      content: Faker::Lorem.sentence(word_count: 100)
     )
 end
 
 5.times do
-    emily.articles.create(
-        title: Faker::Lorem.sentence(word_count: 5),
-        content: Faker::Lorem.sentence(word_count: 100)
+    carlos.articles.create(
+      title: Faker::Lorem.sentence(word_count: 5),
+      content: Faker::Lorem.sentence(word_count: 100)
     )
 end
